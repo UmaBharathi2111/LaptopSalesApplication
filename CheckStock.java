c1200a3ddea155244f320d2624bcdc4e7c48019f
UmaBharathi2111 <146568956+UmaBharathi2111@users.noreply.github.com>
2024-05-23 11:26:09 +0530

    CheckStock.java

--- CheckStock.java
+++ CheckStock.java
@@ -1,42 +1,18 @@
-/*import java.util.*;
-public class CheckStock
-
-{
-      static double Price;
-      public boolean available( String brand,String graphicCard,String processor,String ramSize, String romSize,String screenSize)
-      {
-        
-        boolean result = false;
-        List<LaptopConfigurations> L = new LaptopStock().availableLaptops();
-        for(LaptopConfigurations l : L)
-        {
-            if(brand.equalsIgnoreCase(l.brand) && graphicCard.equalsIgnoreCase  (l.graphicCard) && processor.equalsIgnoreCase(l.processor) && ramSize.equalsIgnoreCase(l.ramSize) && screenSize.equalsIgnoreCase(l.screenSize))
-            {
-                result = true;
-                Price = l.getPrice();
-            }
-        }
-        return result;
-      }
-    }*/
-
 import java.util.*;
 public class CheckStock
 {
-      static double Price;
+      publicic double Price;
       public boolean available( String brand,String graphicCard,String processor,String ramSize, String romSize,String screenSize)
       {
-        
-        boolean result = false;
         List<LaptopConfigurations> L = new LaptopStock().availableLaptops();
         for(LaptopConfigurations l : L)
         {
             if(brand.equalsIgnoreCase(l.brand) && graphicCard.equalsIgnoreCase  (l.graphicCard) && processor.equalsIgnoreCase(l.processor) && ramSize.equalsIgnoreCase(l.ramSize) && screenSize.equalsIgnoreCase(l.screenSize))
             {
-                result = true;
                 Price = l.getPrice();
+                result = true;
             }
         }
-        return result;
+        return false;
       }
    }
